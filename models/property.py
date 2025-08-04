b03fc92caffde78eb94af15e66ce26cb594fca06
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-04 19:48:11 +0300

    Add Smart Buttons

--- models/property.py
+++ models/property.py
@@ -157,6 +157,17 @@ class Property(models.Model):
             'default_property_id': self.id,
         }
         return action
+
+    def action_open_related_owner(self):
+        action = self.env['ir.actions.actions']._for_xml_id('app_one.owner_action')
+        view_id = self.env.ref('app_one.owner_view_form').id
+        action['res_id'] = self.owner_id.id
+        action['views'] = [[view_id,'form']]
+        return action
+
+
+
+
     # @api.depends('owner_id')
     # def _compute_owner_address(self):
     #     for rec in self:
