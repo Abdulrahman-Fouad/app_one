6b7a13a0d2710cbbe7deac292e308bde5f652e0a
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-05 10:49:46 +0300

    APIs P2 (CRUD) 1.Create Operation P2

--- controllers/property_api.py
+++ controllers/property_api.py
@@ -8,9 +8,33 @@ class PropertyApi(http.Controller):
     def post_property(self):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
+        # separate method
+        if not vals.get("name"):
+            return request.make_json_response({
+                "message": "Field name is required",
+            }, status=400)
+        try:
+            res = request.env['property'].sudo().create(vals)
+            if res:
+                return request.make_json_response({
+                    "message": f"{res.name} has been created successfully",
+                    "id": res.id,
+                    "name": res.name,
+                }, status = 201)
+        except Exception as error:
+            return request.make_json_response({
+                "message": error,
+            }, status=400)
+
+
+    @http.route("/v1/property/json", methods=["POST"], type="json", auth="none", csrf=False)
+    def post_property_json(self):
+        args = request.httprequest.data.decode()
+        vals = json.loads(args)
         print(vals)
         res = request.env['property'].sudo().create(vals)
         if res:
-            return request.make_json_response({
+            return {
                 "message": f"{vals["name"]} has been created successfully"
-            }, status = 200)
+            }
+
