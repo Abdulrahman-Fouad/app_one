b1841bd1b0661e0d4e35153b21b48e469e8504ca
abdulrahman <abdulrahmanfo25@gmail.com>
2025-08-05 17:21:37 +0300

    APIs P3 (CRUD) 2. Update Operation

--- controllers/property_api.py
+++ controllers/property_api.py
@@ -1,10 +1,10 @@
 import json
-
 from odoo import http
 from odoo.http import request
 
+
 class PropertyApi(http.Controller):
-    @http.route("/v1/property", methods=["POST"], type="http", auth="none", csrf= False)
+    @http.route("/v1/property", methods=["POST"], type="http", auth="none", csrf=False)
     def post_property(self):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
@@ -20,21 +20,40 @@ class PropertyApi(http.Controller):
                     "message": f"{res.name} has been created successfully",
                     "id": res.id,
                     "name": res.name,
-                }, status = 201)
+                }, status=201)
         except Exception as error:
             return request.make_json_response({
                 "message": error,
             }, status=400)
 
+    @http.route("/v1/property/<int:property_id>", methods=["PUT"], type="http", auth="none", csrf=False)
+    def update_property(self, property_id):
+        try:
+            property = request.env['property'].sudo().search([('id', '=', property_id)])
+            if not property:
+                return request.make_json_response({
+                "message": f"There is no property with id: {property_id} !!!",
+            }, status=400)
+            args = request.httprequest.data.decode()
+            vals = json.loads(args)
+            property.write(vals)
+            return request.make_json_response({
+                "message": f"{property.name} has been updated successfully",
+                "id": property.id,
+                "name": property.name,
+            }, status=200)
+        except Exception as error:
+            return request.make_json_response({
+                "message": error,
+            }, status=400)
 
-    @http.route("/v1/property/json", methods=["POST"], type="json", auth="none", csrf=False)
-    def post_property_json(self):
-        args = request.httprequest.data.decode()
-        vals = json.loads(args)
-        print(vals)
-        res = request.env['property'].sudo().create(vals)
-        if res:
-            return {
-                "message": f"{vals["name"]} has been created successfully"
-            }
-
+    # @http.route("/v1/property/json", methods=["POST"], type="json", auth="none", csrf=False)
+    # def post_property_json(self):
+    #     args = request.httprequest.data.decode()
+    #     vals = json.loads(args)
+    #     print(vals)
+    #     res = request.env['property'].sudo().create(vals)
+    #     if res:
+    #         return {
+    #             "message": f"{vals["name"]} has been created successfully"
+    #         }
